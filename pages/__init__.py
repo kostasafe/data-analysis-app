465076caf9fb94cdbf0b87454277777327c2d63d
Afentoulis Konstantinos <kostasgoume@hotmail.com>
2024-05-02 02:20:24 +0300

    Add files via upload

---
+++ pages/__init__.py
@@ -0,0 +1,6 @@
+from pages.home import show_Home
+# from pages.TwoD_Visualization import show_TwoD_Visualization
+# from pages.First_Machine_Learning_Algorithm import show_First_Machine_Learning_Algorithm
+# from pages.Second_Machine_Learning_Algorithm import show_Second_Machine_Learning_Algorithm
+# from pages.Comparison import show_Comparison
+# from pages.Info import show_Info
---
+++ pages/home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def show_Home(): 
+        st.header("Home")
+        st.write( "ablalalalalal")
+
+
+
+
+
+
+
+
+
+
+
