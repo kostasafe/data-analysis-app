774379e4723df5926ef954dae5e04a7235b06570
Afentoulis Konstantinos <kostasgoume@hotmail.com>
2024-05-07 02:26:32 +0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,11 +1,18 @@
 import streamlit as st
+import panda as pd
+import csv
+from io import StringIO
 
 def show_Home(): 
-        st.header("Home")
-        st.write( "ablalalalalal")
+        st.header("Καλωσήρθατε στην Εφαρμογή Εξόρυξης και Ανάλυσης δεδομένων!")
+        st.write( "Παρακαλώ φορτώστε ένα αρχείο:")
 
 
-
+uploaded_file = st.file_uploader("Choose a CSV or Excel file")
+        if uploaded_file is not None:
+                # To read file as bytes:
+                bytes_data = uploaded_file.getvalue()
+                st.write(bytes_data)
 
 
 
