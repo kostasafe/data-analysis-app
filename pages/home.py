bb759e74ced94d9910032e9ece8228f4574cfefd
Afentoulis Konstantinos <kostasgoume@hotmail.com>
2024-05-07 02:29:35 +0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -6,13 +6,11 @@ from io import StringIO
 def show_Home(): 
         st.header("Καλωσήρθατε στην Εφαρμογή Εξόρυξης και Ανάλυσης δεδομένων!")
         st.write( "Παρακαλώ φορτώστε ένα αρχείο:")
-
-
-uploaded_file = st.file_uploader("Choose a CSV or Excel file")
-        if uploaded_file is not None:
-                # To read file as bytes:
-                bytes_data = uploaded_file.getvalue()
-                st.write(bytes_data)
+        uploaded_file = st.file_uploader("Choose a CSV or Excel file")
+                if uploaded_file is not None:
+                        # To read file as bytes:
+                        bytes_data = uploaded_file.getvalue()
+                        st.write(bytes_data)
 
 
 
