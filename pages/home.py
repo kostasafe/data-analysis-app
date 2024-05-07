a34c29c95de543c94fbbc7369652c3e9cac66a3e
Kostas Afentoulis <[email protected]>
2024-05-08 01:05:03 +0300

    dataframe now visible and working

--- pages/home.py
+++ pages/home.py
@@ -2,32 +2,27 @@ import streamlit as st
 import csv
 import pandas as pd
 from io import StringIO
+import os
 
 
 def show_Home(): 
         st.header("Καλωσήρθατε στην Εφαρμογή Εξόρυξης και Ανάλυσης δεδομένων!")
         st.write( "Παρακαλώ φορτώστε ένα αρχείο:")
-        uploaded_file = st.file_uploader("Choose a CSV or Excel file")
+        uploaded_file = st.file_uploader("Choose a CSV or Excel file", type={"csv", "excel"})
         if uploaded_file is not None:
                 # To read file as bytes:
-                bytes_data = uploaded_file.getvalue()
-                st.write(bytes_data)
+                #bytes_data = uploaded_file.getvalue()
+                generate_button = st.button('Πατήστε εδώ για προβολή του dataset ως λίστα!')
+                if generate_button:
+                        new_list = load_data(uploaded_file)
+                        st.write(new_list)  
+
+                        
 
 
+def load_data(uploaded_file):   
+        if uploaded_file.name.endswith('.csv'):
+                df = pd.read_csv(uploaded_file)
+                return df
 
-
-def load_data(filename):   #NOT WORKING YET
-        thelist = []
-        with open(filename) as test_csv:
-                test_csv_data = csv.reader(test_csv, delimiter=',')
-                next (test_csv_data) #skip header
-                for row in test_csv_data:
-                        thelist.append(row)
-                return thelist
-
-
-
-#new_list = load_data("country_full.csv")  HOW TO CALL THE FUNCTION
-#    for row in new_list:
-#         print(row)
 
