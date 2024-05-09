0720a6474097e8b3095c50e16fc0523d51ad414a
devpersi <109619062+devpersi@users.noreply.github.com>
2024-05-09 17:05:43 +0300

    fix information tab, add more info

--- pages/Info.py
+++ pages/Info.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 def show_Info(): 
         st.header("Οδηγίες χρήσης")
-        st.write("Οδηγίες\nΚι άλλες οδηγίες\nΟδηγιέισον")
+        st.write("Οδηγίες  \nΚι άλλες οδηγίες  \nΟδηγιέισον")
+        st.header("Πληροφορίες")
+        st.write("Αυτή η εφαρμογή δημιουργήθηκε για την εκπλήρωση του μαθήματος [Τεχνολογία Λογισμικού](https://opencourses.ionio.gr/courses/DDI259/)")
+        st.header("Credits")
+        st.write("[devpersi](https://github.com/devpersi):  \nInfo, ...  \n\n[kostasafe](https://github.com/kostasafe):  \nProject setup, 2D visualization")
