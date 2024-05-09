ae30392a3a9dc08298f2739bb93f0480746d090a
devpersi <109619062+devpersi@users.noreply.github.com>
2024-05-09 16:53:10 +0300

    make information tab

---
+++ pages/Info.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_Info(): 
+        st.header("Οδηγίες χρήσης")
+        st.write("Οδηγίες\nΚι άλλες οδηγίες\nΟδηγιέισον")
--- pages/__init__.py
+++ pages/__init__.py
@@ -3,4 +3,4 @@ from pages.TwoD_Visualization import show_TwoD_Visualization
 # from pages.First_Machine_Learning_Algorithm import show_First_Machine_Learning_Algorithm
 # from pages.Second_Machine_Learning_Algorithm import show_Second_Machine_Learning_Algorithm
 # from pages.Comparison import show_Comparison
-# from pages.Info import show_Info
+from pages.Info import show_Info
